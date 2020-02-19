de238679ec953b91ab0baf186077952c46dbc13c
Дмитрий Долиденок <dolidenok@MacBook-Air-Dmitrij.local>
2020-02-19 22:41:53 +0300

    Program ready

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        long amountInKopecks = 110000;
+        long limit = 100000;
+        long bonusPercent = 1;
+        long bonus;
+        if (amountInKopecks > limit) {
+            bonus = amountInKopecks * bonusPercent / 100 / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
     }
 }
